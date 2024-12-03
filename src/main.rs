f303d388f74743fca6bd4ba21785d1c8c5b0ad9a
Perceval ARENOU <perceval.arenou@bilberry.io>
2024-12-03 14:46:26 +0000

    start benching

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+// #![feature(test)]
+
 // include function one from one file and call it in main function
 mod day_1;
 mod day_2;
-
+mod day_3;
 fn main() {
     println!("Starting advent of code");
+    println!("day one");
     day_1::solve_one();
     day_1::solve_two();
 
+    println!("day two");
     day_2::solve_one();
     day_2::solve_two();
+
+    println!("day three");
+    day_3::solve_one();
+
 }
