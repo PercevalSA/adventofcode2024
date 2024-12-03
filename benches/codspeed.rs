f4dd542ae493910ecc990040c9d99741756de5e7
PercevalSA <perceval.arenou@gmail.com>
2024-12-03 19:11:34 +0000

    run bench on day 2

--- benches/codspeed.rs
+++ benches/codspeed.rs
@@ -1,23 +1,11 @@
-use adventofcode2024::day2::solve_one;
+use adventofcode2024::day2::part1;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n - 1) + fibonacci(n - 2),
-    }
-}
-
-pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
-}
-
 pub fn criterion_bench_day_two_first(c: &mut Criterion) {
-    c.bench_function("day 2 first", |b| b.iter(|| black_box(solve_one())));
+    c.bench_function("day 2 first", |b| b.iter(|| black_box(part1())));
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, criterion_bench_day_two_first);
 criterion_main!(benches);
 
 // fn bench_two(bench: &mut Bencher) {
