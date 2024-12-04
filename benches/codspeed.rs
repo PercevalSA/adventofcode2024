0cddbb3cff8f421e43e8e3c86e3e7377ff8bd13b
PercevalSA <perceval.arenou@gmail.com>
2024-12-04 00:09:50 +0000

    and better bench

--- benches/codspeed.rs
+++ benches/codspeed.rs
@@ -2,8 +2,11 @@ use adventofcode2024::{day2, day3, utils};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn bench_day_2(c: &mut Criterion) {
-    c.bench_function("day 2 part 1", |b| b.iter(|| day2::part1()));
-    c.bench_function("day 2 part 2", |b| b.iter(|| day2::part2()));
+    let data_str = utils::read_file("2");
+    let data = data_str.as_str();
+
+    c.bench_function("day 2 part 1", |b| b.iter(|| day2::part1(black_box(data))));
+    c.bench_function("day 2 part 2", |b| b.iter(|| day2::part2(black_box(data))));
 }
 
 pub fn bench_day_3(c: &mut Criterion) {
