23a41f76ec71f352de2229f8fc486cba3afc9ecd
Perceval ARENOU <perceval.arenou@bilberry.io>
2024-12-05 17:12:39 +0000

    starting day5

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,11 @@ extern crate aoc_runner;
 #[macro_use]
 extern crate aoc_runner_derive;
 
-pub mod day1;
-pub mod day2;
-pub mod day3;
-pub mod day4;
 pub mod utils;
+pub mod day1;
+mod day2;
+mod day3;
+mod day4;
+mod day5;
 
 aoc_lib! { year = 2024 }
