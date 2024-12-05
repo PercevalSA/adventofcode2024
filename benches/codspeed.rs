aeba84c2b649bdf8f540805335fac40c5d988a73
Perceval ARENOU <perceval.arenou@bilberry.io>
2024-12-05 15:49:27 +0000

    bench day 4

--- benches/codspeed.rs
+++ benches/codspeed.rs
@@ -1,4 +1,4 @@
-use adventofcode2024::{day2, day3, utils};
+use adventofcode2024::{day2, day3, day4, utils};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn bench_day_2(c: &mut Criterion) {
@@ -17,13 +17,13 @@ pub fn bench_day_3(c: &mut Criterion) {
     c.bench_function("day 3 part 2", |b| b.iter(|| day3::part2(black_box(data))));
 }
 
-// pub fn bench_day_4(c: &mut Criterion) {
-//     let data_str = utils::read_file("day3");
-//     let data = data_str.as_str();
+pub fn bench_day_4(c: &mut Criterion) {
+    let data_str = utils::read_file("day3");
+    let data = data_str.as_str();
 
-//     c.bench_function("day 4 part 1", |b| b.iter(|| day4::part1(black_box(data))));
-//     // c.bench_function("day 4 part 2", |b| b.iter(|| day4::part2(black_box(data))));
-// }
+    c.bench_function("day 4 part 1", |b| b.iter(|| day4::part1(black_box(data))));
+    // c.bench_function("day 4 part 2", |b| b.iter(|| day4::part2(black_box(data))));
+}
 
 criterion_group!(benches, bench_day_2, bench_day_3);
 criterion_main!(benches);
