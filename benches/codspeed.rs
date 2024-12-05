95beb1fba86a0d4a26a656b3443e33dbe558147b
Perceval ARENOU <perceval.arenou@bilberry.io>
2024-12-05 16:56:36 +0000

    bench day 4

--- benches/codspeed.rs
+++ benches/codspeed.rs
@@ -20,9 +20,10 @@ pub fn bench_day_3(c: &mut Criterion) {
 pub fn bench_day_4(c: &mut Criterion) {
     let data_str = utils::read_file("day4");
     let data = day4::parse_input(data_str.as_str());
+    let data2 = day4::parse_input_part2(data_str.as_str());
 
     c.bench_function("day 4 part 1", |b| b.iter(|| day4::part1(black_box(&data))));
-    // c.bench_function("day 4 part 2", |b| b.iter(|| day4::part2(black_box(data))));
+    c.bench_function("day 4 part 2", |b| b.iter(|| day4::part2(black_box(&data2))));
 }
 
 criterion_group!(benches, bench_day_2, bench_day_3, bench_day_4);
